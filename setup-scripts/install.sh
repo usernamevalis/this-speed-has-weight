4dd16b3ae9f8ce75c7acc5251a785cb023aad062
nathano <nathanogates@gmail.com>
2018-04-09 18:52:33 +0200

    change case sensitivity

--- setup-scripts/install.sh
+++ setup-scripts/install.sh
@@ -9,7 +9,7 @@ yes Y | sudo apt-get install npm
 echo "done."
 
 echo "install installing project dependencies"
-npm install ./../project-template/webserver/
+npm install ./../project-template/webserver/ --prefix ./../project-template/webserver/
 
 echo "Downloading Arduino Ide to current folder"
 wget https://downloads.arduino.cc/arduino-1.8.5-linuxarm.tar.xz
@@ -17,7 +17,7 @@ echo "unzipingn Arduino Archive"
 tar -xvf arduino-1.8.5-linuxarm.tar.xz
 echo "Installing Arduino IDE"
 sudo mv arduino-1.8.5 /opt/ && rm arduino-1.8.5-linuxarm.tar.xz
-/opt/arduino-1.8.5/./install.sh
+sudo /opt/arduino-1.8.5/./install.sh
 echo "done."
 
 echo "making a backup of the project template, and placing it in 'backups'"
