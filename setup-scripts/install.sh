4c487a07edb0b520428c868c6cc6fff2389452a9
nathan <nathanogates@gmail.com>
2018-04-10 05:17:34 +0000

    Update install.sh

--- setup-scripts/install.sh
+++ setup-scripts/install.sh
@@ -9,16 +9,22 @@ yes Y | sudo apt-get install npm
 echo "done."
 
 echo "install installing project dependencies"
-npm install ./../project-template/webserver/ --prefix ../project-template/webserver/
+npm --prefix "$DIR/../project-template/webserver/" install  "$DIR/../project-template/webserver/"
 
 echo "Downloading Arduino Ide to current folder"
 wget https://downloads.arduino.cc/arduino-1.8.5-linuxarm.tar.xz
-echo "unzipingn Arduino Archive"
+echo "unzipping Arduino Archive"
 tar -xvf arduino-1.8.5-linuxarm.tar.xz
 echo "Installing Arduino IDE"
 sudo mv arduino-1.8.5 /opt/ && rm arduino-1.8.5-linuxarm.tar.xz
 sudo /opt/arduino-1.8.5/./install.sh
 echo "done."
+
+echo "Backing up project template into 'backups' folder"
+cp -r "$DIR/../project-template" "$DIR/../backups/"
+
+echo "Moving master archive to backups"
+mv "$DIR/../../master.zip"  "$DIR/../backups/" 
 
 echo "done and done."
 echo "goodbye"
